0ccf94a6e732f8589979296892cea0bea85a1b5c
Aaditya <aadityarevandkar#gmail.com>
2024-11-30 21:29:00 +0530

    new3

---
+++ Leetcode/392.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int sp = 0;
+        int tp = 0;
+
+        while (sp < s.length() && tp < t.length()) {
+            if (s[sp] == t[tp]) {
+                sp++;
+            }
+            tp++;
+        }
+
+        return sp == s.length();    
+    }
+};
